e0b08dc541ab111e2ae70c2dee9fcd5c9be53275
HeLiJia <2047298757@qq.com>
2021-04-18 17:12:46 +0800

    '项目初始化'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center-1"><slot name="center-1"></slot></div>
+    <div class="center-2"><slot name="center-2"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "NavBar"
+}
+</script>
+
+<style scoped>
+.nav-bar{
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100,100,100,.1);
+}
+.left, .right{
+  flex: 1;
+
+}
+.center-1,.center-2{
+  flex: 1;
+
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,53 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="left">推荐</div>
+      <div slot="center-1">分类</div>
+    </nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
 </template>
 
 <script>
-export default {
-name: "Home"
+  import NavBar from "../../components/common/navbar/NavBar";
+  import {getHomeMultidate} from "../../network/home";
+
+  //轮播图
+  import {Swiper,SwiperItem} from '../../components/common/swiper/index'
+
+  export default {
+    name: "Home",
+      components:{
+       NavBar,
+       Swiper,
+       SwiperItem
+     },
+     data(){
+        return{
+           banners:[],
+           recommends:[]
+        }
+     },
+    created() {
+     //请求多个数据
+    getHomeMultidate().then(res=>{
+      console.log(res);
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list;
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color:var(--color-tint);
+  color: white;
+}
 </style>
