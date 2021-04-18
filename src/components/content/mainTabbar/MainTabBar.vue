84cce2f09ff0b9365a5e7c81d7dc01b3de964d64
HeLiJia <2047298757@qq.com>
2021-04-18 12:05:51 +0800

    '初始化项目'

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="Teal">
+      <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/dianji.jpg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/course" activeColor="Teal">
+      <img slot="item-icon" src="~assets/img/tabbar/kecheng.jpg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/dianji.jpg" alt="">
+      <div slot="item-text">课程</div>
+    </tab-bar-item>
+    <tab-bar-item path="task" activeColor="Teal">
+      <img slot="item-icon" src="~assets/img/tabbar/zuoye.jpg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/dianji.jpg" alt="">
+      <div slot="item-text">作业</div>
+    </tab-bar-item>
+    <tab-bar-item path="profile" activeColor="Teal">
+      <img slot="item-icon" src="~assets/img/tabbar/my.jpg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/dianji.jpg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "./tabbar/TabBar"
+import TabBarItem from "./tabbar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
